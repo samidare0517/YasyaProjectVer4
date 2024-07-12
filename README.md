4a2330607d3f5fb02bc729c50f950266987837c2
samidare0517 <114457364+samidare0517@users.noreply.github.com>
2024-07-12 09:38:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YasyaProjectVer4
+
+Developed with Unreal Engine 5
---
+++ Source/YasyaProjectVer4/YasyaProjectVer4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "YasyaProjectVer4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YasyaProjectVer4, "YasyaProjectVer4" );
